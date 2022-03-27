160867c4748edb672dcc54b20ec992beaf0161e6
everythingandnothingdev <everythingandnothingdev@gmail.com>
2022-03-27 15:57:16 -0400

    Build for android

---
+++ android/app/src/main/java/com/everythingandnothingdev/webbrowsermediaremotecontrol/MainActivity.java
@@ -0,0 +1,5 @@
+package com.everythingandnothingdev.webbrowsermediaremotecontrol;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/components/remote-trackpad.vue
+++ src/components/remote-trackpad.vue
@@ -1,7 +1,8 @@
 <template>
     <div
         class="remote-trackpad"
-        @pointerdown="onPointerDownTrackpad">
+        @pointerdown="onPointerDownTrackpad"
+        @touchstart="onTouchStartTrackpad">
         <div class="remote-trackpad__icons">
             <ion-icon :icon="chevronUp"/><br>
             Scroll<br>
@@ -25,19 +26,27 @@ export default defineComponent({
     setup() {
         let pointerStartId: number | null = null;
         let pointerStart: DOMPoint | null = null;
+        let lastPreviewDeltaX: number = 0;
+        let lastPreviewDeltaY: number = 0;
 
         onMounted(() => {
             window.addEventListener('pointerup', onPointerUpDocument, true);
             window.addEventListener('pointermove', onPointerMoveDocument, true);
+            window.addEventListener('touchmove', onTouchMoveDocument, true);
+            window.addEventListener('touchend', onTouchEndDocument, true);
         });
 
         onUnmounted(() => {
             window.removeEventListener('pointerup', onPointerUpDocument);
             window.removeEventListener('pointermove', onPointerMoveDocument);
+            window.removeEventListener('touchmove', onTouchMoveDocument);
+            window.removeEventListener('touchend', onTouchEndDocument);
         });
 
         const previewScrollDelta = throttle((deltaX: number, deltaY: number) => {
             if (pointerStart) {
+                lastPreviewDeltaX = deltaX;
+                lastPreviewDeltaY = deltaY;
                 mqttPublish('scrollController', JSON.stringify({ state: 'preview', dx: deltaX, dy: deltaY, t: window.performance.now() }));
             }
         }, 25);
@@ -47,35 +56,61 @@ export default defineComponent({
         }
 
         function onPointerDownTrackpad(event: PointerEvent) {
-            pointerStartId = event.pointerId;
-            pointerStart = new DOMPoint(event.pageX, event.pageY);
+            if (['mouse', 'pen'].includes(event.pointerType)) {
+                pointerStartId = event.pointerId;
+                pointerStart = new DOMPoint(event.pageX, event.pageY);
+            }
+        }
+        function onTouchStartTrackpad(event: TouchEvent) {
+            pointerStartId = event.touches[0].identifier;
+            pointerStart = new DOMPoint(event.touches[0].pageX, event.touches[0].pageY);
         }
 
         function onPointerMoveDocument(event: PointerEvent) {
-            if (pointerStartId != null && event.pointerId === pointerStartId && pointerStart) {
-                const deltaX = event.pageX - pointerStart.x;
-                const deltaY = -(event.pageY - pointerStart.y);
+            if (['mouse', 'pen'].includes(event.pointerType)) {
+                if (pointerStartId != null && event.pointerId === pointerStartId && pointerStart) {
+                    const deltaX = event.pageX - pointerStart.x;
+                    const deltaY = -(event.pageY - pointerStart.y);
+                    previewScrollDelta(deltaX * store.state.scrollSensitivity, deltaY * store.state.scrollSensitivity);
+                }
+            }
+        }
+        function onTouchMoveDocument(event: TouchEvent) {
+            if (pointerStartId != null && event.touches[0]?.identifier === pointerStartId && pointerStart) {
+                const deltaX = event.touches[0].pageX - pointerStart.x;
+                const deltaY = -(event.touches[0].pageY - pointerStart.y);
                 previewScrollDelta(deltaX * store.state.scrollSensitivity, deltaY * store.state.scrollSensitivity);
             }
         }
 
         function onPointerUpDocument(event: PointerEvent) {
-            if (event.pointerId === pointerStartId) {
-                if (pointerStart) {
-                    const deltaX = event.pageX - pointerStart.x;
-                    const deltaY = -(event.pageY - pointerStart.y);
-                    commitScrollDelta(deltaX * store.state.scrollSensitivity, deltaY * store.state.scrollSensitivity);
-                } else {
-                    commitScrollDelta(0, 0);
+            if (['mouse', 'pen'].includes(event.pointerType)) {
+                if (event.pointerId === pointerStartId) {
+                    if (pointerStart) {
+                        const deltaX = event.pageX - pointerStart.x;
+                        const deltaY = -(event.pageY - pointerStart.y);
+                        commitScrollDelta(deltaX * store.state.scrollSensitivity, deltaY * store.state.scrollSensitivity);
+                    } else {
+                        commitScrollDelta(0, 0);
+                    }
+                    pointerStartId = null;
+                    pointerStart = null;
                 }
-                pointerStartId = null;
-                pointerStart = null;
+            }
+        }
+        function onTouchEndDocument(event: TouchEvent) {
+            if (Array.from(event.touches).filter(touch => touch.identifier === pointerStartId).length === 0) {
+                if (pointerStart) {
+                    commitScrollDelta(lastPreviewDeltaX, lastPreviewDeltaY);
+                    pointerStartId = null;
+                    pointerStart = null;
+                }
             }
         }
 
         return {
             chevronUp, chevronDown,
-            onPointerDownTrackpad
+            onPointerDownTrackpad, onTouchStartTrackpad
         };
     }
 });
